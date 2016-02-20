bd6ce08c8e6487f3223b183c15ca6209e87a5106
yedaodao <404069912@qq.com>
2016-02-20 21:17:12 +0800

    update example

--- examples/dev/entry.js
+++ examples/dev/entry.js
@@ -1,2 +1,10 @@
 var createjs = require('../../index!../lib/easeljs-0.8.2.combined').createjs;
-console.log(createjs);
+
+var stage = new createjs.Stage('stage');
+
+var circle = new createjs.Shape();
+circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+circle.x = 100;
+circle.y = 100;
+stage.addChild(circle);
+stage.update();
